120479b488b54a61e21ad5e59079f2c1dafbe6bd
Shyam Kr Baruah <shyamkrbaruahgi@gmail.com>
2025-06-29 02:59:10 +0530

    create api

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,60 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { gql } from 'graphql-tag';
+
+// Schema Definition
+const typeDefs = gql`
+  type User {
+    id: ID!
+    email: String!
+    name: String!
+  }
+
+  type AuthPayload {
+    token: String!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+
+  type Mutation {
+    register(name: String!, email: String!, password: String!): User!
+    login(email: String!, password: String!): AuthPayload!
+  }
+`;
+
+// In-memory user store
+const users: { id: number; name: string; email: string; password: string }[] = [];
+
+// Resolvers
+const resolvers = {
+  Query: {
+    users: () => users,
+  },
+  Mutation: {
+    register: (_: any, { name, email, password }: any) => {
+      const newUser = { id: Date.now(), name, email, password };
+      users.push(newUser);
+      return newUser;
+    },
+    login: (_: any, { email, password }: any) => {
+      const user = users.find((u) => u.email === email && u.password === password);
+      if (!user) {
+        throw new Error('Invalid credentials');
+      }
+      const token = Buffer.from(`${email}:${password}`).toString('base64');
+      return { token };
+    },
+  },
+};
+
+// Apollo Server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Export Vercel-compatible route handlers
+export const GET = startServerAndCreateNextHandler(server);
+export const POST = startServerAndCreateNextHandler(server);
--- lib/apolloclient.ts
+++ lib/apolloclient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  uri: "/api/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
